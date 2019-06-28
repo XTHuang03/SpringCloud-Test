3406bab5f2592af3c718de345c5b1034d88a1d53
UID <UID@DESKTOP-DACJOCN>
2019-06-28 10:05:14 +0800

    1、冲突同步完成

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-	//这里修改
 	private Integer id;
 	private String name;
 	private Integer number;
-	//asds
+	
+	//哈哈哈哈
 }
